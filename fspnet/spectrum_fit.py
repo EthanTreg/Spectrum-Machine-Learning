Added batch size to config.yaml. Added PyXspec iterations to config.yaml. Improved linear weights mapping calculation. Improved autoencoder saliency plot. Fixed pair plot not working for one dataset. Fixed open_config crashing if environment is not script/package.

fspnet/spectrum_fit.py
@@ -4,6 +4,7 @@ Main spectral machine learning module
 import os
 import pickle
 import logging as log
+from time import time
 
 import torch
 import matplotlib
@@ -12,8 +13,8 @@ from torch.utils.data import DataLoader
 
 from fspnet.utils.data import data_initialisation
 from fspnet.utils.utils import open_config, get_device
-from fspnet.utils.training import training, pyxspec_test
 from fspnet.utils.network import load_network, Network
+from fspnet.utils.training import training, pyxspec_test
 from fspnet.utils.analysis import (
     autoencoder_saliency,
     decoder_saliency,
@@ -29,9 +30,77 @@ from fspnet.utils.plots import (
 )
 
 
-def predict_parameters(
-        config: dict | str = '../config.yaml',
-        encoder: Network = None) -> np.ndarray:
+def pyxspec_tests(config: dict, dataset: torch.utils.data.Dataset):
+    """
+    Tests the PGStats of the different fitting methods using PyXspec
+
+    Parameters
+    ----------
+    config : dictionary
+        Configuration dictionary
+    dataset : Dataset
+        Validation dataset to test the PGStats for
+    """
+    # Initialize variables
+    cpus = config['training']['cpus']
+    default_params = config['model']['default-parameters']
+    log_params = config['model']['log-parameters']
+    predictions_path = config['output']['parameter-predictions-path']
+    worker_dir = config['output']['worker-directory']
+    worker_data = {
+        'optimize': False,
+        'dirs': [
+            os.path.dirname(os.path.abspath(__file__)),
+            config['data']['spectra-directory'],
+        ],
+        'iterations': config['model']['iterations'],
+        'fix_params': config['model']['fixed-parameters'],
+        'model': config['model']['model-name'],
+        'custom_model': config['model']['custom-model-name'],
+        'model_dir': config['model']['model-directory'],
+    }
+    indices = dataset.indices
+    names = dataset.dataset.names[indices]
+    xspec_params = dataset.dataset.params[indices]
+    default_params = np.repeat([default_params], names.size, axis=0)
+
+    # Untransform Xspec parameters
+    xspec_params = xspec_params * dataset.dataset.transform[1][1] + dataset.dataset.transform[1][0]
+
+    if log_params:
+        xspec_params[:, log_params] = 10 ** xspec_params[:, log_params]
+
+    # Save worker variables
+    with open(f'{worker_dir}worker_data.pickle', 'wb') as file:
+        pickle.dump(worker_data, file)
+
+    # Encoder validation performance
+    print('\nTesting Encoder...')
+    pyxspec_test(worker_dir, predictions_path, cpus=cpus, job_name='Encoder_output')
+
+    # Xspec performance
+    print('\nTesting Xspec...')
+    pyxspec_test(worker_dir, (names, xspec_params), cpus=cpus, job_name='Xspec_output')
+
+    # Default performance
+    print('\nTesting Defaults...')
+    pyxspec_test(worker_dir, (names, default_params), cpus=cpus)
+
+    # Allow Xspec optimization
+    worker_data['optimize'] = True
+    with open(f'{worker_dir}worker_data.pickle', 'wb') as file:
+        pickle.dump(worker_data, file)
+
+    # Encoder + Xspec performance
+    print('\nTesting Encoder + Fitting...')
+    pyxspec_test(worker_dir, predictions_path, cpus=cpus, job_name='Encoder_Xspec_output')
+
+    # Default + Xspec performance
+    print('\nTesting Defaults + Fitting...')
+    pyxspec_test(worker_dir, (names, default_params), cpus=cpus, job_name='Default_Xspec_output')
+
+
+def predict_parameters(config: dict | str = '../config.yaml') -> np.ndarray:
     """
     Predicts parameters using the encoder & saves the results to a file
 
@@ -39,9 +108,6 @@ def predict_parameters(
     ----------
     config : dictionary | string, default = '../config.yaml'
         Configuration dictionary or file path to the configuration file
-    encoder : Network, default = None
-        Encoder network for generating parameters,
-        otherwise will load network from the configuration file
 
     Returns
     -------
@@ -51,15 +117,15 @@ def predict_parameters(
     if isinstance(config, str):
         _, config = open_config('spectrum-fit', config)
 
-    returns = initialization(
+    if config['training']['encoder-save']:
+        config['training']['encoder-load'] = config['training']['encoder-save']
+
+    (_, loader), encoder = initialization(
         config['training']['encoder-name'],
         config,
     )[2:]
-    _, loader = returns[0]
 
-    if not encoder:
-        encoder = returns[1]
-
+    initial_time = time()
     output_path = config['output']['parameter-predictions-path']
     names = []
     params = []
@@ -72,17 +138,16 @@ def predict_parameters(
     with torch.no_grad():
         for spectra, *_, names_batch in loader:
             names.extend(names_batch)
-            param_batch = encoder(spectra.to(get_device()[1])).cpu()
-
-            # Transform parameters
-            param_batch = param_batch * param_transform[1] + param_transform[0]
-
-            if log_params:
-                param_batch[:, log_params] = 10 ** param_batch[:, log_params]
-
+            param_batch = encoder(spectra.to(get_device()[1]))
             params.append(param_batch)
 
-    output = np.hstack((np.expand_dims(names, axis=1), torch.cat(params).numpy()))
+    params = torch.cat(params).cpu().numpy() * param_transform[1] + param_transform[0]
+
+    if log_params:
+        params[:, log_params] = 10 ** params[:, log_params]
+
+    output = np.hstack((np.expand_dims(names, axis=1), params))
+    print(f'Parameter prediction time: {time() - initial_time:.3e} s')
     np.savetxt(output_path, output, delimiter=',', fmt='%s')
 
     return output
@@ -132,12 +197,13 @@ def initialization(
 
     # Load config parameters
     load_num = config['training'][f'{network_type}-load']
-    num_params = config['model']['parameters-number']
+    batch_size = config['training']['batch-size']
     learning_rate = config['training']['learning-rate']
     networks_dir = config['training']['network-configs-directory']
     spectra_path = config['data'][f'{network_type}-data-path']
-    states_dir = config['output']['network-states-directory']
+    num_params = config['model']['parameters-number']
     log_params = config['model']['log-parameters']
+    states_dir = config['output']['network-states-directory']
 
     if load_num:
         try:
@@ -156,6 +222,7 @@ def initialization(
     loaders = data_initialisation(
         spectra_path,
         log_params,
+        batch_size=batch_size,
         transform=transform,
         indices=indices,
     )
@@ -190,26 +257,13 @@ def main(config_path: str = '../config.yaml'):
     # Training variables
     tests = config['training']['tests']
     num_epochs = config['training']['epochs']
-    cpus = config['training']['cpus']
 
     # Data paths
     e_data_path = config['data']['encoder-data-path']
     d_data_path = config['data']['decoder-data-path']
 
     # Model parameters
-    default_params = config['model']['default-parameters']
     param_names = config['model']['parameter-names']
-    worker_data = {
-        'optimize': False,
-        'dirs': [
-            os.path.dirname(os.path.abspath(__file__)),
-            config['data']['spectra-directory'],
-        ],
-        'fix_params': config['model']['fixed-parameters'],
-        'model': config['model']['model-name'],
-        'custom_model': config['model']['custom-model-name'],
-        'model_dir': config['model']['model-directory'],
-    }
 
     # Output paths
     predictions_path = config['output']['parameter-predictions-path']
@@ -242,10 +296,6 @@ def main(config_path: str = '../config.yaml'):
     if not os.path.exists(states_dir):
         os.makedirs(states_dir)
 
-    # Save worker variables
-    with open(f'{worker_dir}worker_data.pickle', 'wb') as file:
-        pickle.dump(worker_data, file)
-
     # Initialize data & decoder
     d_initial_epoch, d_losses, d_loaders, decoder = initialization(
         config['training']['decoder-name'],
@@ -268,7 +318,6 @@ def main(config_path: str = '../config.yaml'):
         states_dir=states_dir,
         losses=d_losses,
     )
-
     plot_training('Decoder', plots_dir, *decoder_return)
 
     # Train encoder
@@ -281,87 +330,35 @@ def main(config_path: str = '../config.yaml'):
         losses=e_losses,
         surrogate=decoder,
     )
-
     plot_training('Autoencoder', plots_dir, *encoder_return)
 
     # Plot linear weight mappings
     plot_linear_weights(config, linear_weights(decoder))
 
     # Generate parameter predictions
-    predict_parameters(config=config, encoder=encoder)
-    plot_param_comparison(
-        plots_dir,
-        param_names,
-        config,
-    )
+    predict_parameters(config=config)
+    plot_param_comparison(param_names, config)
     plot_param_distribution(
         'Decoder_Param_Distribution',
         [d_data_path, predictions_path],
-        ['Target', 'Prediction'],
         config,
+        ['Target', 'Prediction'],
     )
     plot_param_distribution(
         'Encoder_Param_Distribution',
         [e_data_path, predictions_path],
-        ['Target', 'Prediction'],
         config,
+        ['Target', 'Prediction'],
     )
-    plot_param_pairs([e_data_path, predictions_path], ['Targets', 'Predictions'], config)
+    plot_param_pairs((e_data_path, predictions_path), config, labels=('Targets', 'Predictions'))
 
     # Calculate saliencies
     decoder_saliency(d_loaders[1], decoder)
     saliency_output = autoencoder_saliency(e_loaders[1], encoder, decoder)
     plot_saliency(plots_dir, *saliency_output)
 
-    if not tests:
-        return
-
-    # Encoder validation performance
-    print('\nTesting Encoder...')
-    pyxspec_test(
-        worker_dir,
-        e_loaders[1],
-        cpus=cpus,
-        job_name='Encoder_output',
-        encoder=encoder,
-    )
-
-    # Xspec performance
-    print('\nTesting Xspec...')
-    pyxspec_test(worker_dir, e_loaders[1], cpus=cpus, job_name='Xspec_output')
-
-    # Default performance
-    print('\nTesting Defaults...')
-    pyxspec_test(
-        worker_dir,
-        e_loaders[1],
-        cpus=cpus,
-        defaults=torch.tensor(default_params)
-    )
-
-    # Allow Xspec optimization
-    worker_data['optimize'] = True
-    with open(f'{worker_dir}worker_data.pickle', 'wb') as file:
-        pickle.dump(worker_data, file)
-
-    # Encoder + Xspec performance
-    print('\nTesting Encoder + Fitting...')
-    pyxspec_test(
-        worker_dir,
-        e_loaders[1],
-        cpus=cpus,
-        job_name='Encoder_Xspec_output',
-        encoder=encoder,
-    )
-
-    # Default + Xspec performance
-    print('\nTesting Defaults + Fitting...')
-    pyxspec_test(
-        worker_dir,
-        e_loaders[1],
-        cpus=cpus,
-        defaults=torch.tensor(default_params)
-    )
+    if tests:
+        pyxspec_tests(config, e_loaders[1].dataset)
 
 
 if __name__ == '__main__':

fspnet/utils/analysis.py
@@ -8,8 +8,8 @@ import numpy as np
 from numpy import ndarray
 from torch.utils.data import DataLoader
 
-from fspnet.utils.data import load_data
 from fspnet.utils.network import Network
+from fspnet.utils.data import load_params
 from fspnet.utils.utils import get_device
 
 
@@ -95,8 +95,8 @@ def decoder_saliency(loader: DataLoader, decoder: Network):
     params_std = torch.std(saliency, dim=0) / torch.min(saliency_params)
 
     print(
-        f'\nParameter impact on decoder:\n{params_impact.tolist()}'
-        f'\nSaliency spread:\n{params_std.tolist()}\n'
+        f"\nParameter impact on decoder:\n{[f'{x:.2f}' for x in params_impact]}"
+        f"\nSaliency spread:\n{[f'{x:.2f}' for x in params_std]}\n"
     )
 
 
@@ -114,25 +114,14 @@ def param_comparison(data_paths: tuple[str, str]) -> tuple[ndarray, ndarray]:
     tuple[ndarray, ndarray]
         Target parameters and parameter predictions
     """
-    params = []
     names = []
+    params = []
 
     # Load config parameters
     for data_path in data_paths:
-        data = load_data(data_path, load_kwargs={'dtype': str})
-
-        if '.pickle' in data_path:
-            if 'names' in data:
-                names.append(np.array(data['names']))
-            else:
-                names.append(np.arange(len(data['params']), dtype=float).astype(str))
-
-            data = np.array(data['params'])
-        elif '.csv' in data_path:
-            names.append(data[:, 0])
-            data = data[:, 1:].astype(float)
-
-        params.append(data)
+        returns = load_params(data_path, load_kwargs={'dtype': str})
+        names.append(returns[0])
+        params.append(returns[1])
 
     # Sort for longest dataset first
     sort_idx = np.argsort([param.shape[0] for param in params])[::-1]
@@ -140,9 +129,9 @@ def param_comparison(data_paths: tuple[str, str]) -> tuple[ndarray, ndarray]:
     names = [names[i] for i in sort_idx]
 
     # Sort target spectra by name
-    sort_idx = np.argsort(names[0])
-    names[0] = names[0][sort_idx]
-    params[0] = params[0][sort_idx]
+    name_sort_idx = np.argsort(names[0])
+    names[0] = names[0][name_sort_idx]
+    params[0] = params[0][name_sort_idx]
 
     # Filter target parameters using spectra that was predicted and log parameters
     target_idx = np.searchsorted(names[0], names[1])
@@ -151,13 +140,14 @@ def param_comparison(data_paths: tuple[str, str]) -> tuple[ndarray, ndarray]:
     params[0] = params[0][shuffle_idx]
     params[1] = params[1][shuffle_idx]
 
-    return np.swapaxes(params[0][:1000], 0, 1), np.swapaxes(params[1][:1000], 0, 1)
+    params = [params[i] for i in sort_idx]
+
+    return np.swapaxes(params[0], 0, 1), np.swapaxes(params[1], 0, 1)
 
 
 def linear_weights(network: Network) -> ndarray:
     """
-    Returns the mapping of the weights from the lowest dimension
-    to a high dimension for the 3 smallest linear layers
+    Returns the mapping of all linear weights from the input to the output
 
 
     If the low dimension is 5 and the high dimension is 240,
@@ -181,20 +171,19 @@ def linear_weights(network: Network) -> ndarray:
 
     # Get all linear weights in the network
     for name, param in network.named_parameters():
-        if re.search(r'.*linear_\d.weight', name):
-            weights.append(param)
+        if re.search(r'.*linear_\d+.weight', name):
+            weights.append(param.detach().cpu().numpy())
 
-    weights_idx = np.argsort([weight.numel() for weight in weights])
-    weights = [weights[idx].detach().cpu().numpy() for idx in weights_idx]
+    # Calculate weight mapping for each input parameter
+    for param_weight in np.swapaxes(weights[0], 0, 1):
+        # Loop through each layer except the first and multiple weight mapping between layers
+        for i, layer_weights in enumerate(weights[1:]):
+            # If second layer, multiply by input weight, otherwise multiple all weight mappings
+            if i == 0:
+                weight = np.sum(param_weight * layer_weights, axis=1)
+            else:
+                weight = np.sum(weight * layer_weights, axis=1)
 
-    for weights_1l in np.swapaxes(weights[0], 1, 0):
-        weights_i = []
-        weights_1l = np.repeat(weights_1l[np.newaxis], weights[1].shape[0], axis=0)
-
-        for weights_3i in weights[2]:
-            weights_3i = np.repeat(weights_3i[:, np.newaxis], weights[1].shape[1], axis=1)
-            weights_i.append(np.sum(weights_1l * weights_3i * weights[1]))
-
-        param_weights.append(weights_i)
+        param_weights.append(weight)
 
     return np.array(param_weights)

fspnet/utils/data.py
@@ -223,6 +223,38 @@ def load_data(data_path: str, load_kwargs: dict = None) -> dict | ndarray:
     return data
 
 
+def load_params(data_path: str, load_kwargs: dict = None) -> tuple[ndarray, ndarray]:
+    """
+    Loads parameters and spectra names from file, supports .csv, .pickle and .npy
+
+    Parameters
+    ----------
+    data_path : string
+        Path to the parameters
+    load_kwargs : dictionary, default = None
+        Optional arguments to pass into np.loadtxt for .csv
+
+    Returns
+    -------
+    tuple[ndarray, ndarray]
+        Names and parameters
+    """
+    data = load_data(data_path, load_kwargs=load_kwargs)
+
+    if '.pickle' in data_path:
+        if 'names' in data:
+            names = np.array(data['names'])
+        else:
+            names = np.arange(len(data['params']), dtype=float).astype(str)
+
+        params = np.array(data['params'])
+    elif '.csv' in data_path:
+        names = data[:, 0]
+        params = data[:, 1:].astype(float)
+
+    return names, params
+
+
 def load_x_data(y_size: int) -> ndarray:
     """
     Fetches x data from file and matches the length to the y data
@@ -319,6 +351,7 @@ def delete_data(directory: str = None, files: list[str] = None):
 def data_initialisation(
         spectra_path: str,
         log_params: list,
+        batch_size: int = 120,
         val_frac: float = 0.1,
         transform: tuple[tuple[float, float], tuple[ndarray, ndarray]] = None,
         indices: ndarray = None) -> tuple[DataLoader, DataLoader]:
@@ -331,6 +364,9 @@ def data_initialisation(
         Path to synthetic data
     log_params : list
         Index of each free parameter in logarithmic space
+    batch_size : integer, default = 1024
+        Number of data inputs per weight update,
+        smaller values update the network faster and requires less memory, but is more unstable
     val_frac : float, default = 0.1
         Fraction of validation data
     transform : tuple[tuple[float, float], tuple[ndarray, ndarray]], default = None
@@ -343,7 +379,6 @@ def data_initialisation(
     tuple[DataLoader, DataLoader]
         Dataloaders for the training and validation datasets
     """
-    batch_size = 120
     kwargs = get_device()[0]
 
     # Fetch dataset & create train & val data

fspnet/utils/layers.py
@@ -240,7 +240,7 @@ def linear(kwargs: dict, layer: dict) -> dict:
     kwargs : dictionary
         i : integer
             Layer number;
-        data_size : integer
+        data_size : list[integer]
             Hidden layer output length;
         dims : list[integer]
             Dimensions in each layer, either linear output features or convolutional/GRU filters;
@@ -259,6 +259,8 @@ def linear(kwargs: dict, layer: dict) -> dict:
             if output_size from kwargs and factor is provided, features will not be used;
         dropout : boolean, default = False
             If dropout should be used;
+        batch_norm : boolean, default = 0
+            If batch normalisation should be used;
         activation : boolean, default = True
             If SELU activation should be used;
 
@@ -296,7 +298,7 @@ def convolutional(kwargs: dict, layer: dict) -> dict:
     kwargs : dictionary
         i : integer
             Layer number;
-        data_size : integer
+        data_size : list[integer]
             Hidden layer output length;
         dims : list[integer]
             Dimensions in each layer, either linear output features or convolutional/GRU filters;
@@ -367,17 +369,18 @@ def convolutional(kwargs: dict, layer: dict) -> dict:
 
 def recurrent(kwargs: dict, layer: dict) -> dict:
     """
-    Gated recurrent unit (GRU) layer constructor
+    Recurrent layer constructor for either RNN, GRU or LSTM
 
     Parameters
     ----------
     kwargs : dictionary
         i : integer
             Layer number;
-        data_size : integer
+        data_size : list[integer]
             Hidden layer output length;
         dims : list[integer]
-            Dimensions in each layer, either linear output features or convolutional/GRU filters;
+            Dimensions in each layer, either linear output features or
+            convolutional/recurrent filters;
         module : Sequential
             Sequential module to contain the layer;
         dropout_prob : float, optional
@@ -385,15 +388,19 @@ def recurrent(kwargs: dict, layer: dict) -> dict:
     layer : dictionary
         dropout : boolean, default = True
             If dropout should be used;
+        batch_norm : boolean, default = 0
+            If batch normalisation should be used;
         activation : boolean, default = True
             If ELU activation should be used;
         layers : integer, default = 2
             Number of stacked GRU layers;
-        factor : float, default = 1
-            Output size of the layer depending on the network's output size;
+        filters : integer, default = 1
+            Number of output filters;
+        method : string, default = gru
+            Type of recurrent layer, can be gru, lstm or rnn;
         bidirectional : string, default = None
-            If a bidirectional GRU should be used and method for combining the two directions,
-            can be sum, mean or concatenation;
+            If a bidirectional recurrence should be used and
+            method for combining the two directions, can be sum, mean or concatenation;
 
     Returns
     -------
@@ -450,51 +457,10 @@ def recurrent(kwargs: dict, layer: dict) -> dict:
         RecurrentOutput(recurrent_layer, bidirectional=bidirectional)
     )
 
-    _optional_layer(True, 'activation', kwargs, layer, nn.ELU())
     _optional_layer(False, 'batch_norm', kwargs, layer, nn.BatchNorm1d(kwargs['dims'][-1]))
+    _optional_layer(True, 'activation', kwargs, layer, nn.ELU())
 
     kwargs['data_size'].append(kwargs['data_size'][-1])
-
-    return kwargs
-
-
-def linear_upscale(kwargs: dict, _: dict) -> dict:
-    """
-    Constructs a 2x upscaler using a linear layer,
-    combines reshape for use within convolutional layers
-
-    Parameters
-    ----------
-    kwargs : dictionary
-        i : integer
-            Layer number;
-        data_size : integer
-            Hidden layer output length;
-        dims : list[integer]
-            Dimensions in each layer, either linear output features or convolutional/GRU filters;
-        module : Sequential
-            Sequential module to contain the layer;
-    _ : dictionary
-        For compatibility
-
-    Returns
-    -------
-    dictionary
-        Returns the input kwargs with any changes made by the function
-    """
-    linear_layer = nn.Linear(
-        in_features=kwargs['data_size'][-1],
-        out_features=kwargs['data_size'][-1] * 2,
-    )
-
-    kwargs['module'].add_module(f"reshape_{kwargs['i']}", Reshape([-1]))
-    kwargs['module'].add_module(f"linear_{kwargs['i']}", linear_layer)
-    kwargs['module'].add_module(f"SELU_{kwargs['i']}", nn.SELU())
-    kwargs['module'].add_module(f"reshape_{kwargs['i']}", Reshape([1, -1]))
-
-    # Data size doubles
-    kwargs['data_size'].append(kwargs['data_size'][-1] * 2)
-    kwargs['dims'].append(1)
 
     return kwargs
 
@@ -508,7 +474,7 @@ def conv_upscale(kwargs: dict, layer: dict) -> dict:
     kwargs : dictionary
         i : integer
             Layer number;
-        data_size : integer
+        data_size : list[integer]
             Hidden layer output length;
         dims : list[integer], optional
             Dimensions in each layer, either linear output features or convolutional/GRU filters;
@@ -516,7 +482,7 @@ def conv_upscale(kwargs: dict, layer: dict) -> dict:
             Sequential module to contain the layer;
     layer : dictionary
         filters : integer
-            Number of convolutional filters, must be a multiple of 2;
+            Number of output convolutional filters;
         batch_norm : boolean, default = False
             If batch normalisation should be used;
         activation : boolean, default = True
@@ -532,6 +498,7 @@ def conv_upscale(kwargs: dict, layer: dict) -> dict:
     layer['dropout'] = False
     layer['stride'] = 1
     layer['padding'] = 'same'
+    layer['filters'] *= 2
     kwargs = convolutional(kwargs, layer)
 
     # Upscaling done using pixel shuffling
@@ -553,7 +520,7 @@ def conv_transpose(kwargs: dict, layer: dict) -> dict:
     kwargs : dictionary
         i : integer
             Layer number;
-        data_size : integer
+        data_size : list[integer]
             Hidden layer output length;
         dims : list[integer]
             Dimensions in each layer, either linear output features or convolutional/GRU filters;
@@ -607,7 +574,7 @@ def upsample(kwargs: dict, _: dict) -> dict:
     kwargs : dictionary
         i : integer
             Layer number;
-        data_size : integer
+        data_size : list[integer]
             Hidden layer output length;
         dims : list[integer]
             Dimensions in each layer, either linear output features or convolutional/GRU filters;
@@ -659,6 +626,7 @@ def conv_depth_downscale(kwargs: dict, layer: dict) -> dict:
     layer['dropout'] = False
     layer['filters'] = 1
     layer['kernel'] = 1
+    layer['stride'] = 1
     layer['padding'] = 'same'
 
     convolutional(kwargs, layer)
@@ -675,7 +643,7 @@ def conv_downscale(kwargs: dict, layer: dict) -> dict:
     kwargs : dictionary
         i : integer
             Layer number;
-        data_size : integer
+        data_size : list[integer]
             Hidden layer output length;
         dims : list[integer]
             Dimensions in each layer, either linear output features or convolutional/GRU filters;
@@ -716,7 +684,7 @@ def pool(kwargs: dict, _: dict) -> dict:
     kwargs : dictionary
         i : integer
             Layer number;
-        data_size : integer
+        data_size : list[integer]
             Hidden layer output length;
         dims : list[integer]
             Dimensions in each layer, either linear output features or convolutional/GRU filters;
@@ -749,7 +717,7 @@ def reshape(kwargs: dict, layer: dict) -> dict:
     kwargs : dictionary
         i : integer
             Layer number;
-        data_size : integer
+        data_size : list[integer]
             Hidden layer output length;
         dims : list[integer]
             Dimensions in each layer, either linear output features or convolutional/GRU filters;
@@ -794,7 +762,7 @@ def sample(kwargs: dict, layer: dict) -> dict:
     kwargs : dictionary
         i : integer
             Layer number;
-        data_size : integer
+        data_size : list[integer]
             Hidden layer output length;
         dims : list[integer]
             Dimensions in each layer, either linear output features or convolutional/GRU filters;
@@ -836,7 +804,7 @@ def extract(kwargs: dict, layer: dict) -> dict:
     Parameters
     ----------
     kwargs : dictionary
-        data_size : integer
+        data_size : list[integer]
             Hidden layer output length;
         dims : list[integer]
             Dimensions in each layer, either linear output features or convolutional/GRU filters;
@@ -861,6 +829,8 @@ def clone(kwargs: dict, _: dict) -> dict:
     Parameters
     ----------
     kwargs : dictionary
+        data_size : list[integer]
+            Hidden layer output length;
         dims : list[integer]
             Dimensions in each layer, either linear output features or convolutional/GRU filters;
     _ : dictionary
@@ -883,6 +853,8 @@ def concatenate(kwargs: dict, layer: dict) -> dict:
     Parameters
     ----------
     kwargs : dictionary
+        data_size : list[integer]
+            Hidden layer output length;
         dims : list[integer]
             Dimensions in each layer, either linear output features or convolutional/GRU filters;
     layer : dictionary
@@ -906,6 +878,8 @@ def shortcut(kwargs: dict, layer: dict) -> dict:
     Parameters
     ----------
     kwargs : dictionary
+        data_size : list[integer]
+            Hidden layer output length;
         dims : list[integer]
             Dimensions in each layer, either linear output features or convolutional/GRU filters;
     layer : dictionary
@@ -933,6 +907,8 @@ def skip(kwargs: dict, layer: dict) -> dict:
     Parameters
     ----------
     kwargs : dictionary
+        data_size : list[integer]
+            Hidden layer output length;
         dims : list[integer]
             Dimensions in each layer, either linear output features or convolutional/GRU filters;
     layer : dictionary

fspnet/utils/plots.py
@@ -10,14 +10,14 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from fspnet.utils.utils import subplot_grid
 from fspnet.utils.analysis import param_comparison
-from fspnet.utils.data import load_data, load_x_data, data_normalization
+from fspnet.utils.data import load_params, load_x_data
 
 MAJOR = 24
 MINOR = 20
 FIG_SIZE = (16, 9)
 
 
-def _legend(labels: ndarray, columns: int = 2):
+def _legend(labels: ndarray, columns: int = 2) -> matplotlib.legend.Legend:
     """
     Plots a legend across the top of the plot
 
@@ -27,6 +27,11 @@ def _legend(labels: ndarray, columns: int = 2):
         Legend matplotlib handels and labels as an array to be unpacked into handels and labels
     columns : integer, default = 2
         Number of columns for the legend
+
+    Returns
+    -------
+    Legend
+        Legend object
     """
     legend = plt.figlegend(
         *labels,
@@ -34,13 +39,14 @@ def _legend(labels: ndarray, columns: int = 2):
         ncol=columns,
         bbox_to_anchor=(0.5, 0.91),
         fontsize=MAJOR,
-        columnspacing=10,
     )
     legend.get_frame().set_alpha(None)
 
     for handle in legend.legendHandles:
         if isinstance(handle, matplotlib.collections.PathCollection):
             handle.set_sizes([100])
+
+    return legend
 
 
 def _initialize_plot(
@@ -193,18 +199,20 @@ def _plot_reconstruction(
     axis.hlines(0, xmin=np.min(x_data), xmax=np.max(x_data), color='k')
 
     divider = make_axes_locatable(axis)
-    axis_2 = divider.append_axes('top', size='150%', pad=0)
+    major_axis = divider.append_axes('top', size='150%', pad=0)
 
-    axis_2.scatter(x_data, y_data, label='Spectrum')
-    axis_2.scatter(x_data, y_recon, label='Reconstruction')
-    axis_2.locator_params(axis='y', nbins=3)
-    axis_2.tick_params(axis='y', labelsize=MINOR)
-    axis_2.set_xticks([])
+    major_axis.scatter(x_data, y_data, label='Spectrum')
+    major_axis.scatter(x_data, y_recon, label='Reconstruction')
+    major_axis.locator_params(axis='y', nbins=3)
+    major_axis.tick_params(axis='y', labelsize=MINOR)
+    major_axis.set_xticks([])
 
-    return axis_2
+    return major_axis
 
 
-def _plot_reconstructions(spectra: ndarray, outputs: ndarray):
+def _plot_reconstructions(
+        spectra: ndarray,
+        outputs: ndarray) -> tuple[list[plt.Axes], list[plt.Axes], matplotlib.legend.Legend]:
     """
     Plots reconstructions and residuals for 4 spectra
 
@@ -214,7 +222,13 @@ def _plot_reconstructions(spectra: ndarray, outputs: ndarray):
         True spectra
     outputs : ndarray
         Predicted spectra
+
+    Returns
+    -------
+    tuple[list[Axes], list[Axes], Legend]
+        Major axes, minor axes and legend
     """
+    major_axes = []
     x_data = load_x_data(spectra.shape[-1])
 
     # Initialize reconstructions plots
@@ -229,10 +243,15 @@ def _plot_reconstructions(spectra: ndarray, outputs: ndarray):
 
     # Plot reconstructions
     for spectrum, output, axis in zip(spectra, outputs, axes):
-        main_axis = _plot_reconstruction(x_data, spectrum, output, axis)
+        major_axes.append(_plot_reconstruction(x_data, spectrum, output, axis))
 
-    labels = np.hstack((main_axis.get_legend_handles_labels(), axes[0].get_legend_handles_labels()))
-    _legend(labels, columns=3)
+    labels = np.hstack((
+        major_axes[0].get_legend_handles_labels(),
+        axis.get_legend_handles_labels()),
+    )
+    legend = _legend(labels, columns=3)
+
+    return major_axes, axes, legend
 
 
 def _plot_histogram(
@@ -311,76 +330,65 @@ def plot_saliency(plots_dir: str, spectra: ndarray, predictions: ndarray, salien
         Saliency
     """
     # Constants
-    cmap = plt.cm.hot
+    bins = 8
     x_data = load_x_data(spectra.shape[1])
 
-    x_regions = x_data[::12]
-    saliencies = np.mean(saliencies.reshape(saliencies.shape[0], -1, 12), axis=-1)
-    saliencies = data_normalization(saliencies, mean=False, axis=1)[0] * 0.9 + 0.05
+    x_regions = x_data[::bins]
+    saliencies = np.mean(saliencies.reshape(saliencies.shape[0], -1, bins), axis=-1)
 
     # Initialize saliency plots
-    axes = _initialize_plot(
-        (2, 2),
-        legend=True,
-        x_label='Energy (keV)',
-        gridspec_kw={'left': 0.01, 'bottom': 0.05, 'hspace': 0, 'wspace': 0},
-        plot_kwargs={'sharex': 'col'},
-    ).flatten()
+    major_axes, minor_axes, legend = _plot_reconstructions(spectra, predictions)
+    legend.remove()
 
-    # Plot each saliency map
-    for i, (
-            axis,
-            spectrum,
-            prediction,
-            saliency
-    ) in enumerate(zip(axes, spectra, predictions, saliencies)):
-        for j, (x_region, saliency_region) in enumerate(zip(x_regions[:-1], saliency[:-1])):
-            axis.axvspan(x_region, x_regions[j + 1], color=cmap(saliency_region))
+    for axis, saliency in zip(major_axes, saliencies):
+        twin_axis = axis.twinx()
+        twin_axis.step(x_regions, saliency, color='green', label='Saliency')
+        twin_axis.tick_params(right=False, labelright=False)
 
-        axis.axvspan(x_regions[-1], x_data[-1], color=cmap(saliency[-1]))
-
-        axis.scatter(x_data, spectrum, label='Target')
-        axis.scatter(x_data, prediction, color='g', label='Prediction')
-        axis.text(0.96, 0.9, i + 1, fontsize=MAJOR, transform=axis.transAxes)
-        axis.tick_params(left=False, labelleft=False, labelbottom=False, bottom=False)
-
-    _legend(axes[0].get_legend_handles_labels())
+    labels = np.hstack((
+        axis.get_legend_handles_labels(),
+        twin_axis.get_legend_handles_labels(),
+        minor_axes[0].get_legend_handles_labels()),
+    )
+    _legend(labels, columns=4)
     plt.savefig(f'{plots_dir}Saliency_Plot.png', transparent=False)
 
 
-def plot_param_pairs(data_paths: list[str], labels: list[str], config: dict):
+def plot_param_pairs(
+        data_paths: tuple[str] | tuple[str, str],
+        config: dict,
+        labels: tuple[str, str] = None):
     """
     Plots a pair plot to compare the distributions and comparisons between parameters
 
     Parameters
     ----------
-    data_paths : list[string]
+    data_paths : tuple[string] | tuple[string, string]
         Paths to the parameters to compare, can have length 1 or 2
-    labels : list[string]
-        Labels for the data of the same length as data_paths
     config : dictionary
         Configuration dictionary
+    labels : tuple[string, string], default = None
+        Labels for the legend if provided
     """
     log_params = config['model']['log-parameters']
     param_names = config['model']['parameter-names']
     plots_dir = config['output']['plots-directory']
 
+    if not labels:
+        labels = [None]
+
     # Load data & shuffle
     if len(data_paths) == 2:
         params = param_comparison(data_paths)
     else:
-        params = load_data(data_paths[0], load_kwargs={'usecols': range(1, 6)})
-
-        if '.pickle' in data_paths[0]:
-            params = np.array(params['params'])
-
+        params = load_params(data_paths[0], load_kwargs={'usecols': range(1, 6)})[1]
         np.random.shuffle(params)
         params = [np.swapaxes(params, 0, 1)]
 
     # Initialize pair plots
     axes = _initialize_plot(
         (params[0].shape[0],) * 2,
-        legend=True,
+        legend=labels[0],
         x_label=' ',
         y_label=' ',
         plot_kwargs={'sharex': 'col'},
@@ -391,6 +399,11 @@ def plot_param_pairs(data_paths: list[str], labels: list[str], config: dict):
     for i, (axes_col, *y_param) in enumerate(zip(axes, *params)):
         for j, (axis, *x_param) in enumerate(zip(axes_col, *params)):
             log = False
+
+            if len(x_param) > 1:
+                data_twin = x_param[1]
+            else:
+                data_twin = None
 
             # Share y-axis for all scatter plots
             if i != j and i == 0:
@@ -425,9 +438,8 @@ def plot_param_pairs(data_paths: list[str], labels: list[str], config: dict):
 
             # Plot scatter plots & histograms
             if i == j:
-                twin_axis = _plot_histogram(x_param[0], axis, log=log, data_twin=x_param[1])
+                twin_axis = _plot_histogram(x_param[0], axis, log=log, data_twin=data_twin)
                 axis.tick_params(labelleft=False, left=False)
-                twin_axis.tick_params(labelright=False, right=False)
             elif j < i:
                 axis.scatter(
                     x_param[0][:1000],
@@ -438,6 +450,9 @@ def plot_param_pairs(data_paths: list[str], labels: list[str], config: dict):
                 )
             else:
                 axis.remove()
+
+            if twin_axis:
+                twin_axis.tick_params(labelright=False, right=False)
 
             if j < i and len(x_param) > 1:
                 axis.scatter(
@@ -454,30 +469,29 @@ def plot_param_pairs(data_paths: list[str], labels: list[str], config: dict):
                 axis.set_xlabel(param_names[j], fontsize=MINOR)
 
             # Left column parameter names
-            if j == 0:
+            if j == 0 and i != 0:
                 axis.set_ylabel(param_names[i], fontsize=MINOR)
 
-    _legend(axes[-1, 0].get_legend_handles_labels())
+    if labels[0]:
+        _legend(axes[-1, 0].get_legend_handles_labels())
+
     plt.savefig(f'{plots_dir}Parameter_Pairs.png')
 
 
-def plot_param_comparison(
-        plots_dir: str,
-        param_names: list[str],
-        config: dict):
+def plot_param_comparison(param_names: list[str], config: dict):
     """
     Plots prediction against target for each parameter
 
     Parameters:
     ----------
-    plots_dir : string
-        Directory to save plots
     param_names : list[string]
         List of parameter names
     config: dictionary
         Configuration dictionary
     """
     log_params = config['model']['log-parameters']
+    plots_dir = config['output']['plots-directory']
+
     target, prediction = param_comparison((
         config['data']['encoder-data-path'],
         config['output']['parameter-predictions-path'],
@@ -493,8 +507,8 @@ def plot_param_comparison(
     for i, (name, axis, target_param, predicted_param) in enumerate(zip(
         param_names,
         axes.values(),
-        target,
-        prediction)):
+        target[:, :1000],
+        prediction[:, :1000])):
         value_range = [
             min(np.min(target_param), np.min(predicted_param)),
             max(np.max(target_param), np.max(predicted_param))
@@ -511,7 +525,11 @@ def plot_param_comparison(
     plt.savefig(f'{plots_dir}Parameter_Comparison.png', transparent=False)
 
 
-def plot_param_distribution(name: str, data_paths: list[str], labels: list[str], config: dict):
+def plot_param_distribution(
+        name: str,
+        data_paths: list[str],
+        config: dict,
+        labels: list[str] = None):
     """
     Plots histogram of each parameter for both true and predicted
 
@@ -521,10 +539,10 @@ def plot_param_distribution(name: str, data_paths: list[str], labels: list[str],
         Name to save the plot
     data_paths : list[string]
         Paths to the parameters to plot
-    labels : list[string]
-        Legend labels for each data path
     config : dictionary
         Configuration dictionary
+    labels : list[string], default = None
+        Legend labels for each data path
     """
     params = []
     log_params = config['model']['log-parameters']
@@ -538,28 +556,38 @@ def plot_param_distribution(name: str, data_paths: list[str], labels: list[str],
     )
 
     for data_path in data_paths:
-        data = load_data(data_path, load_kwargs={'usecols': range(1, 6)})
-
-        if '.pickle' in data_path:
-            data = data['params']
-
-        params.append(np.swapaxes(data, 0, 1))
+        params.append(np.swapaxes(load_params(
+            data_path,
+            load_kwargs={'usecols': range(1, 6)},
+        )[1], 0, 1))
 
     # Plot subplots
     for i, (title, *param, axis) in enumerate(zip(param_names, *params, axes.values())):
         log = False
 
+        if len(param) > 1:
+            data_twin = param[1]
+        else:
+            data_twin = None
+
         if i in log_params:
             axis.set_xscale('log')
             log = True
 
-        twin_axis = _plot_histogram(param[0], axis, log=log, labels=labels, data_twin=param[1])
+        twin_axis = _plot_histogram(param[0], axis, log=log, labels=labels, data_twin=data_twin)
         axis.set_xlabel(title, fontsize=MAJOR)
 
-    _legend(np.hstack((
-        axes[0].get_legend_handles_labels(),
-        twin_axis.get_legend_handles_labels(),
-    )))
+    if twin_axis:
+        labels = np.hstack((
+            axes[0].get_legend_handles_labels(),
+            twin_axis.get_legend_handles_labels(),
+        ))
+    elif labels:
+        labels = axes[0].get_legend_handles_labels()
+
+    if labels:
+        _legend(labels)
+
     plt.savefig(plots_dir + name)
 
 

fspnet/utils/synthesize_worker.py
@@ -184,7 +184,7 @@ def _generate_synth(
 
     # Generate fake spectrum
     model.setPars(params)
-    xspec.AllData.fakeit(nSpectra=1, settings=fake_base, applyStats=False, noWrite=True)
+    xspec.AllData.fakeit(nSpectra=1, settings=fake_base, applyStats=True, noWrite=True)
 
     return np.fromiter(params.values(), dtype=float)[fixed_num:]
 

fspnet/utils/training.py
@@ -10,41 +10,39 @@ from torch.utils.data import DataLoader
 
 from fspnet.utils.network import Network
 from fspnet.utils.utils import get_device
+from fspnet.utils.data import load_params
 from fspnet.utils.multiprocessing import check_cpus, mpi_multiprocessing
 
 
-def _xspec_loss(
+def pyxspec_test(
         worker_dir: str,
-        names: list[str],
-        params: np.ndarray,
+        params: str | tuple[np.ndarray, np.ndarray],
         cpus: int = 1,
-        job_name: str = None) -> float:
+        job_name: str = None):
     """
     Calculates the PGStat loss using PyXspec
-    Done using multiprocessing if >2 cores available
+    Done using multiprocessing if > 2 cores available
 
     Parameters
     ----------
     worker_dir : string
         Directory to where to save worker data
-    names : list[string]
-        Spectra names
-    params : ndarray
-        Parameter predictions from CNN
+    params : string | tuple[ndarray, ndarray]
+        Path to the parameter predictions or tuple of spectra file names and parameter predictions
     cpus : integer, default = 1
         Number of threads to use, 0 will use all available
     job_name : string, default = None
         If not None, file name to save the output to
-
-    Returns
-    -------
-    float
-        Average loss
     """
     # Initialize variables
     data = []
-
     cpus = check_cpus(cpus)
+
+    # Load parameters if file path provided
+    if isinstance(params, str):
+        names, params = load_params(params, load_kwargs={'dtype': str})
+    else:
+        names, params = params
 
     # Divide work between workers
     worker_names = np.array_split(names, cpus)
@@ -69,71 +67,7 @@ def _xspec_loss(
         np.savetxt(f'{worker_dir}{job_name}.csv', data, delimiter=',', fmt='%s')
 
     # Median loss
-    return float(np.median(data[:, -1].astype(float)))
-
-
-def pyxspec_test(
-        worker_dir: str,
-        loader: DataLoader,
-        cpus: int = 1,
-        job_name: str = None,
-        defaults: torch.Tensor = None,
-        encoder: Network = None):
-    """
-    If encoder is provided, creates predictions for each spectrum, otherwise, uses true parameters
-    then calculates loss
-
-    Parameters
-    ----------
-    worker_dir : string
-        Directory to where to save worker data
-    loader : DataLoader
-        PyTorch DataLoader that contains data to train
-    cpus : integer, default = 1
-        Number of threads to use, 0 will use all available
-    job_name : string, default = None
-        If not None, file name to save the output to
-    defaults : Tensor, default = None
-        If default parameters should be used
-    encoder : Network, default = None
-        Encoder to predict parameters, if None, true parameters will be used
-    """
-    # Initialize variables
-    initial_time = time()
-    names = []
-    params = []
-    log_params = loader.dataset.dataset.log_params
-    param_transform = loader.dataset.dataset.transform[1]
-
-    if encoder:
-        encoder.eval()
-
-    # Initialize processes for multiprocessing of encoder loss calculation
-    with torch.no_grad():
-        for data in loader:
-            names.extend(data[-1])
-
-            # Use defaults if provided, else if encoder is provided, generate predictions,
-            # else get true parameters
-            if defaults is not None:
-                param_batch = defaults.repeat(data[0].size(0), 1)
-            elif encoder:
-                param_batch = encoder(data[0].to(get_device()[1])).cpu()
-            else:
-                param_batch = data[1]
-
-            # Transform parameters if not defaults
-            if defaults is None:
-                param_batch = param_batch * param_transform[1] + param_transform[0]
-                param_batch[:, log_params] = 10 ** param_batch[:, log_params]
-
-            params.append(param_batch)
-
-    params = torch.cat(params)
-    print(f'Parameter retrieval time: {time() - initial_time:.3e} s')
-
-    loss = _xspec_loss(worker_dir, names, params.numpy(), cpus=cpus, job_name=job_name)
-    print(f'Reduced PGStat Loss: {loss:.3e}')
+    print(f'Reduced PGStat Loss: {np.median(data[:, -1].astype(float)):.3e}')
 
 
 def train_val(